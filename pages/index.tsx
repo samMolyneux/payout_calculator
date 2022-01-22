efba837b21928c7661b744be9082fdd6569d986f
Sam Molyneux <sam@molyneuxs.net>
2022-01-22 22:19:36 +0000

    centered some stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home(){
-  return <div className="text-2xl text-red-400">Hello World</div>;
+  return( 
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+
+
+    <div className="text-2xl text-red-400 text-center">Hello World</div>
+
+    <button className="p-2 flex w-24 h-16 border bg-blue-500 text-white font-medium rounded hover:font-bold"> I'm a button</button>
+
+    </div>
+
+);
 }
