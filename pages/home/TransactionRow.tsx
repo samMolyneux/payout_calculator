23da8efc3f1aeec4096257213141976bfc092816
Sam Molyneux <sam@molyneuxs.net>
2023-09-07 10:29:58 +0100

    completed file restructure

---
+++ pages/home/TransactionRow.tsx
@@ -0,0 +1,19 @@
+import type React from "react";
+import { Transaction } from "../types/index";
+import { convertToPence, convertToPounds } from "../../scripts/util";
+
+const TransactionRow: React.FC<{
+  transaction: Transaction;
+  key: string;
+}> = (props) => {
+  return (
+    <div className=" flex bg-gray-700 p-1 my-2 rounded items-center h-6">
+      <div className=" flex ">From: {props.transaction.from}</div>
+      <div className=" flex  p-2">To: {props.transaction.to}</div>
+      <div className=" flex  p-2">
+        Amount: £{convertToPounds(props.transaction.val)}
+      </div>
+    </div>
+  );
+};
+export default TransactionRow;
